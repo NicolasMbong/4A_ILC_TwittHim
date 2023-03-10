4672a444cd707d5647c6d3baea8d754ecb3a989a
MbongNicolas <95071643+NicolasMbong@users.noreply.github.com>
2023-03-10 09:40:27 +0100

    Update TwittHim.py

--- TwittHim.py
+++ TwittHim.py
@@ -1,10 +1,10 @@
-# from flask import Flask 
+from flask import Flask 
 import sys
 import csv # bibliothèque pour éditer et lire les fichiers csv
 import datetime # bibliothèque pour utiliser la date et l'heure
 import hashlib # bibliothèque pour utiliser le hachage sha256
 
-# app = Flask(__name__)
+app = Flask(__name__)
 
 
 class User:
